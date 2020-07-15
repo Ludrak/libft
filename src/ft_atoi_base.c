02e668e44636c67d820faedd54b5229fc38a09d2
Ludrak <luca06.robino@gmail.com>
2020-07-15 22:21:23 +0200

    added ft_atoi_base

---
+++ src/ft_atoi_base.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrobino <lrobino@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/02 17:50:20 by lrobino           #+#    #+#             */
+/*   Updated: 2020/07/15 22:20:46 by lrobino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	return (c == ' ' || (c >= '\t' && c <= '\r'));
+}
+
+int			ft_atoi_base(char *str, char *base)
+{
+	char			buf[256];
+	int				i;
+	int				sign;
+	unsigned int	nb;
+
+	ft_memset(buf, -1, 256);
+	i = 0;
+	while (base[i] && base[i] != '+' && base[i] != '-'
+		&& !ft_isspace(base[i]) && buf[base[i]] == -1)
+	{
+		buf[base[i]] = i;
+		i++;
+	}
+	if (base[i])
+		return (0);
+	sign = 1;
+	while (*str == '-' || *str == '+' || ft_isspace(*str))
+		if (*str++ == '-')
+			sign *= -1;
+	nb = 0;
+	while (*str && buf[*str] != -1)
+		nb = (i * nb) + (buf[*str++]);
+	return (nb * sign);
+}
